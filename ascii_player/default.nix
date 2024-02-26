7490cdb98a55f84a9f70bb6b00bd6d7883c97f95
Ferios <exferios@gmail.com>
2024-02-26 14:58:08 +0300

    Updated to new version

--- ascii_player/default.nix
+++ ascii_player/default.nix
@@ -1,20 +1,20 @@
-{ stdenv, fetchgit, lib, cmake, ffmpeg, SDL2, SDL2_ttf }:
+{ pkgs ? import <nixpkgs> {} }:
 
-stdenv.mkDerivation rec {
+pkgs.stdenv.mkDerivation rec {
   pname = "ascii_player";
-  version = "0.1.0";
+  version = "0.1.1";
 
-  src = fetchgit {
+  src = pkgs.fetchgit {
     url = "https://github.com/Ferios/ascii_player.git";
-    rev = "c6d905d6ec4bdde7b82c538fcb8008c74765aefc";
-    sha256 = "sha256-PD84auZQU1crpyUJy2k7UNzKP1JwmKiucv13k1taOAs=";
+    rev = "7bc507c4b8ee8652c831ae99aea8c56fbe916a96";
+    sha256 = "sha256-R7UtAVf+RUtjpzDEe4ovaqAUWvK7dOct0cqZZCSvmXg=";
   };
 
   buildInputs = [
-    cmake
-    ffmpeg
-    SDL2_ttf
-    SDL2
+    pkgs.cmake
+    pkgs.ffmpeg
+    pkgs.SDL2_ttf
+    pkgs.SDL2
   ];
 
   configurePhase = ''
@@ -31,4 +31,18 @@ stdenv.mkDerivation rec {
     cp resources/SpaceMono-Regular.ttf $out/bin
     cp resources/BadApple.mp4 $out/bin
   '';
+
+  meta = with pkgs.lib; {
+    description = "A basic video-to-ascii player";
+    longDescription = ''
+      Video player that uses FFmpeg and SDL to play video files
+      as ASCII-art.
+      Cd to 'ascii_player' directory and execute with a video
+      file (BadApple.mp4 is provided as a reference).
+    '';
+    homepage = "https://github.com/Ferios/ascii_player";
+    maintainers = with maintainers; [ Ferios ];
+    mainProgram = "ascii_player";
+  };
 }
+
